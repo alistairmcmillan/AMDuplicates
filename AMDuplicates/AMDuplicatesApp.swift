ee2fae89d616713504c3946d2e51160c893a9f7d
Alistair McMillan <alistairmcmillan75@gmail.com>
2025-11-15 17:59:02 +0000

    Initial Commit

---
+++ AMDuplicates/AMDuplicatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AMDuplicatesApp.swift
+//  AMDuplicates
+//
+//  Created by Alistair McMillan on 15/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct AMDuplicatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AMDuplicatesTests/AMDuplicatesTests.swift
@@ -0,0 +1,16 @@
+//
+//  AMDuplicatesTests.swift
+//  AMDuplicatesTests
+//
+//  Created by Alistair McMillan on 15/11/2025.
+//
+
+import Testing
+
+struct AMDuplicatesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
